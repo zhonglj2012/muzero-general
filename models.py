15e304b6941a28c6ea69ef96e41a6fc66d9d2c30
zhonglj2012 <56805709+zhonglj2012@users.noreply.github.com>
2022-02-26 17:51:36 +1100

    test

--- models.py
+++ models.py
@@ -198,7 +198,7 @@ class MuZeroFullyConnectedNetwork(AbstractNetwork):
                 x = torch.cat((encoded_state, encoded_state_global_ret), dim=1)
             elif encoded_state.shape[0] > self.encoded_state_global.shape[0]:
                 encoded_state_ret = encoded_state[:self.encoded_state_global.shape[0],:]
-                print(encoded_state_global_ret)
+                print('encoded_state_global_ret',encoded_state_global_ret)
                 x = torch.cat((encoded_state_ret, self.encoded_state_global), dim=1)
             else:
                 x = torch.cat((encoded_state, self.encoded_state_global), dim=1)
